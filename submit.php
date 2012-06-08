ea8a6a5536440ee223eef4a7846572e9dd270f2d
Jason Spriggs <jason@jasonspriggs.com>
2012-06-08 16:28:59 -0400

    Adding Some Style

--- submit.php
+++ submit.php
@@ -7,8 +7,28 @@
 <html>
 	<head>
 		<title><?php echo $option['title']; ?> | Submit</title>
+		<link href="_style/jquery.min.js">
 	</head>
 	<body>
-		
+		<div class="navbar navbar-fixed-top">
+      		<div class="navbar-inner">
+        		<div class="container">
+          			<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+          			</button>
+          			<a class="brand" href="index.php"><?php echo $option['title']; ?></a>
+          			<div class="nav-collapse collapse">
+            			<ul class="nav">
+              				<li class="">
+                				<a href="index.php">Overview</a>
+              				</li>
+              				<li class="divider-vertical"></li>
+            			</ul>
+          			</div>
+        		</div>
+      		</div>
+    	</div>
 	</body>
 </html>
